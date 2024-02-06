7ced5b2e592fae92c57e6371f4a8fcb0b9281a26
Eva Takou <etakou@vt.edu>
2024-02-06 19:06:01 +0200

    updates

--- Functionalities/general_utilities/SWAP_rows.m
+++ Functionalities/general_utilities/SWAP_rows.m
@@ -2,9 +2,8 @@ function  Matrix=SWAP_rows(Matrix,row1,row2)
 %Function to swap rows in a matrix.
 %Input: The matrix, and the rows
 %Output: The matrix with swapped rows.
-temp = Matrix(row1,:);
 
-%Matrix([row2,row1],:)=Matrix([row1,row2],:);
+temp = Matrix(row1,:);
 Matrix(row1,:)=Matrix(row2,:);
 Matrix(row2,:)=temp;
 
