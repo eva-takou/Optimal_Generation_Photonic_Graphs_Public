334b340681b2df9d6d9a69a50256bc865e5508d7
Eva Takou <etakou@vt.edu>
2024-02-06 15:02:30 +0200

    updates

--- Functionalities/Photonic_generation_circuit/utilities/store_updates/store_graph_transform.m
+++ Functionalities/Photonic_generation_circuit/utilities/store_updates/store_graph_transform.m
@@ -1,21 +1,25 @@
-function graphs = store_graph_transform(G,identifier,graphs)
+function graphs = store_graph_transform(Adj,identifier,graphs)
 %Function to store the graphs of the circuit generation in a list.
-%Input: G: the graph to store
+%Input: inp: The adjacency matrix.
 %       identifier: if this after a TRM, a decoupling step, or photon
 %       absorption
 %       graphs: the remaining circuit so far (could be empty)
 %Output: the updated graphs
 
+    
 if isempty(graphs)
 
-    graphs.Adjacency{1}  = G;
+    graphs.Adjacency{1}  = Adj;
     graphs.identifier{1} = identifier;
     
 else
-
-    graphs.Adjacency{end+1}  = G;
+    
+    graphs.Adjacency{end+1}  = Adj;
     graphs.identifier{end+1} = identifier;
     
 end
+    
+    
+
 
 end
