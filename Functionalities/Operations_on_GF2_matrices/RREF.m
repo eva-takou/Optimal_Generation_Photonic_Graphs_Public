57e73d0df8ffcab9d8a04e75b4b6639bee654a3c
Eva Takou <etakou@vt.edu>
2024-02-16 10:39:57 -0500

    speed up updates

--- Functionalities/Operations_on_GF2_matrices/RREF.m
+++ Functionalities/Operations_on_GF2_matrices/RREF.m
@@ -28,7 +28,8 @@ while true
     subTabZ = subTabZ(KU:end);
     
     Ys          = bitand(subTabZ,subTabX);
-    Pauli_Pos_Y = find(Ys,1);
+    Pauli_Pos_Y = find(Ys,1); 
+    k           = Pauli_Pos_Y;
     
     %Remove Ys
     subTabZ = bitxor(subTabZ,Ys); 
@@ -37,31 +38,32 @@ while true
    if sum(subTabX)>0 
       
        Pauli_Pos_X=find(subTabX,1);
+       k=[k,Pauli_Pos_X]; 
        
    else
        
        Pauli_Pos_X=[];
-       
+      
    end
     
     
    if sum(subTabZ)>0
         
         Pauli_Pos_Z=find(subTabZ,1);
+        k=[k,Pauli_Pos_Z];
         
    else
        
        Pauli_Pos_Z=[];
-        
-    end
-
-    k = [Pauli_Pos_X,Pauli_Pos_Y,Pauli_Pos_Z];
-    
-    if isempty(k)
+       
+   end
+   
+    if isempty(k) 
         NL=NL+1;
         continue
     end
 
+    
     if length(k)==1 %Only 1 kind of Pauli Operator for this qubit
 
         k = k + (KU-1); %We redefine k because in the find command the first row is KU.
@@ -69,34 +71,45 @@ while true
                         
         if k~=KU  %SWAP if necessary:
 
-            temp = Tab(k,:);
-            Tab(k,:)=Tab(KU,:);
-            Tab(KU,:)=temp;
+            temp      = Tab(k,:);
+            Tab(k,:)  = Tab(KU,:);
+            Tab(KU,:) = temp;
 
         end
         
-        for ll=(KU+1):K %Multiply row KU with all other rows in the active
-                                  %region that have the same Pauli in column NL.
-                                  
-            xi = Tab(ll,NL);  zi=Tab(ll,NL+n);
-
-            if  xi+zi>0 %non-identity 
-                
-                Tab = rowsum(Tab,n,ll,KU); %Stab update
-                
-            end
-            
-        end
+        SliceTabX = Tab(:,NL);
+        SliceTabZ = Tab(:,NL+n);
+        
+        locs = SliceTabX>0 | SliceTabZ>0;
+        locs = locs(KU+1:end); 
        
+        INDX = KU+1:K;
+        INDX = INDX(locs);
+        
+        for ll=1:length(INDX)
+                
+            Tab = rowsum(Tab,n,INDX(ll),KU);
+            
+        end        
+        
+%         for ll=(KU+1):K %Multiply row KU with all other rows in the active
+%                                   %region that have the same Pauli in column NL.
+%                                   
+%             xi = Tab(ll,NL);  zi=Tab(ll,NL+n);
+%             
+%             if  zi>0 || xi>0 %non-identity  %0.318 for xi+zi>0
+%                  
+%                 Tab = rowsum(Tab,n,ll,KU); %Stab update
+%                 
+%             end
+%             
+%         end
+        
         NL = NL+1;
         KU = KU+1;
     
     else %>=2 kinds of operators XY, YZ, XZ or XZY.
 
-        %temp    = [Pauli_Pos_X;Pauli_Pos_Z;Pauli_Pos_Y];
-        %temp    = sort(temp); %If we don't sort, we might exchange k1 with k2 positions.
-        %k1      = temp(1);    %Pick 1st occurence of non-trivial pauli
-        %k2      = temp(2);
         k1 = min(k); %For unsorted we need to pick min/max
         k2 = max(k);
         
@@ -114,7 +127,6 @@ while true
                 
             end
             
-
         elseif (k1==Pauli_Pos_Y) 
             
             flag1='Y';
@@ -143,7 +155,6 @@ while true
                 
             end
             
-            
         end
 
         k1=k1+(KU-1); 
@@ -157,7 +168,6 @@ while true
             
         end
 
-
         if k2~=(KU+1)
 
             
@@ -167,28 +177,37 @@ while true
         
         end
 
-        for row=KU+2:K %Multiply all other rows in the active region either 
-                       %with row KU,row KU+1, both or none, depending on their element
+        SliceTabX = Tab(:,NL);
+        SliceTabZ = Tab(:,NL+n);
+        
+        locs = SliceTabX>0 | SliceTabZ>0;
+        locs = locs(KU+2:end); 
+       
+        INDX = KU+2:K;
+        INDX = INDX(locs);
+
+        for row=1:length(INDX) %Multiply all other rows in the active region either 
+                               %with row KU,row KU+1, both or none, depending on their element
                        
-            xi = Tab(row,NL); 
-            zi = Tab(row,NL+n);
+            xi = SliceTabX(INDX(row));
+            zi = SliceTabZ(INDX(row));
 
             if xi==1 && zi==1 %Ylocation detected
 
                 if flag1=='Y'
 
-                    Tab=rowsum(Tab,n,row,KU);
+                    Tab=rowsum(Tab,n,INDX(row),KU);
 
                 elseif flag2=='Y'
 
                     
-                    Tab=rowsum(Tab,n,row,(KU+1));
+                    Tab=rowsum(Tab,n,INDX(row),(KU+1));
 
                 else %if (flag1=='X' && flag2=='Z')  || (flag1=='Z' && flag2=='X')
 
                     
-                    Tab=rowsum(Tab,n,row,KU);
-                    Tab=rowsum(Tab,n,row,(KU+1));
+                    Tab=rowsum(Tab,n,INDX(row),KU);
+                    Tab=rowsum(Tab,n,INDX(row),(KU+1));
 
                 end
 
@@ -198,55 +217,138 @@ while true
                 if flag1=='X'
 
                     
-                    Tab=rowsum(Tab,n,row,KU);  %Stab update
+                    Tab=rowsum(Tab,n,INDX(row),KU);  %Stab update
 
                 elseif flag2=='X'
 
                     
-                    Tab=rowsum(Tab,n,row,(KU+1)); %Stab update
+                    Tab=rowsum(Tab,n,INDX(row),(KU+1)); %Stab update
 
                 elseif flag1=='Y' %&& flag2=='Z' %First rowsum with KU and then with KU+1
                     
                     
-                    Tab=rowsum(Tab,n,row,KU);
-                    Tab=rowsum(Tab,n,row,(KU+1));
+                    Tab=rowsum(Tab,n,INDX(row),KU);
+                    Tab=rowsum(Tab,n,INDX(row),(KU+1));
 
                 elseif flag1=='Z' %&& flag2=='Y' %First rowsum with KU+1 and then with KU
 
-                    Tab=rowsum(Tab,n,row,(KU+1));                    
-                    Tab=rowsum(Tab,n,row,KU);
+                    Tab=rowsum(Tab,n,INDX(row),(KU+1));                    
+                    Tab=rowsum(Tab,n,INDX(row),KU);
 
                 end
 
 
 
-            elseif zi==1 %Z location detected
+            else %if zi==1 %Z location detected
 
                 if flag1=='Z'
 
                     
-                    Tab=rowsum(Tab,n,row,KU);
+                    Tab=rowsum(Tab,n,INDX(row),KU);
                     
                 elseif flag2=='Z'
 
-                    Tab=rowsum(Tab,n,row,(KU+1));
+                    Tab=rowsum(Tab,n,INDX(row),(KU+1));
 
                 elseif flag1=='X' %&& flag2=='Y'  %First with Y and then with X
                 
-                    Tab = rowsum(Tab,n,row,(KU+1));
-                    Tab = rowsum(Tab,n,row,KU);
+                    Tab = rowsum(Tab,n,INDX(row),(KU+1));
+                    Tab = rowsum(Tab,n,INDX(row),KU);
 
                 elseif flag1=='Y' %&& flag2=='X' %First with Y and then with X
 
-                    Tab = rowsum(Tab,n,row,KU);
-                    Tab = rowsum(Tab,n,row,(KU+1));                                
+                    Tab = rowsum(Tab,n,INDX(row),KU);
+                    Tab = rowsum(Tab,n,INDX(row),(KU+1));                                
 
                 end
-
 
             end
             
         end
+        
+%         for row=KU+2:K %Multiply all other rows in the active region either 
+%                        %with row KU,row KU+1, both or none, depending on their element
+%                        
+%             xi = SliceTabX(row);
+%             zi = SliceTabZ(row);
+% 
+%             if xi+zi==0 %Adress identities first to exit if statements fast
+%                 
+%                 continue
+%                 
+%             elseif xi==1 && zi==1 %Ylocation detected
+% 
+%                 if flag1=='Y'
+% 
+%                     Tab=rowsum(Tab,n,row,KU);
+% 
+%                 elseif flag2=='Y'
+% 
+%                     
+%                     Tab=rowsum(Tab,n,row,(KU+1));
+% 
+%                 else %if (flag1=='X' && flag2=='Z')  || (flag1=='Z' && flag2=='X')
+% 
+%                     
+%                     Tab=rowsum(Tab,n,row,KU);
+%                     Tab=rowsum(Tab,n,row,(KU+1));
+% 
+%                 end
+% 
+% 
+%             elseif xi==1 %X location detected
+% 
+%                 if flag1=='X'
+% 
+%                     
+%                     Tab=rowsum(Tab,n,row,KU);  %Stab update
+% 
+%                 elseif flag2=='X'
+% 
+%                     
+%                     Tab=rowsum(Tab,n,row,(KU+1)); %Stab update
+% 
+%                 elseif flag1=='Y' %&& flag2=='Z' %First rowsum with KU and then with KU+1
+%                     
+%                     
+%                     Tab=rowsum(Tab,n,row,KU);
+%                     Tab=rowsum(Tab,n,row,(KU+1));
+% 
+%                 elseif flag1=='Z' %&& flag2=='Y' %First rowsum with KU+1 and then with KU
+% 
+%                     Tab=rowsum(Tab,n,row,(KU+1));                    
+%                     Tab=rowsum(Tab,n,row,KU);
+% 
+%                 end
+% 
+% 
+% 
+%             else %if zi==1 %Z location detected
+% 
+%                 if flag1=='Z'
+% 
+%                     
+%                     Tab=rowsum(Tab,n,row,KU);
+%                     
+%                 elseif flag2=='Z'
+% 
+%                     Tab=rowsum(Tab,n,row,(KU+1));
+% 
+%                 elseif flag1=='X' %&& flag2=='Y'  %First with Y and then with X
+%                 
+%                     Tab = rowsum(Tab,n,row,(KU+1));
+%                     Tab = rowsum(Tab,n,row,KU);
+% 
+%                 elseif flag1=='Y' %&& flag2=='X' %First with Y and then with X
+% 
+%                     Tab = rowsum(Tab,n,row,KU);
+%                     Tab = rowsum(Tab,n,row,(KU+1));                                
+% 
+%                 end
+% 
+%             end
+%             
+%         end
 
         NL=NL+1; %next qubit
         KU=KU+2; %+2 rows.
