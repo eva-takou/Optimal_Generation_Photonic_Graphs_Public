8ff4704e945962093fa3b257b0e7d47be37c7fff
Eva Takou <etakou@vt.edu>
2024-02-16 18:56:37 -0500

    efficiency updates

--- Functionalities/Functions_on_Tableaus/adjacency/Get_Adjacency.m
+++ Functionalities/Functions_on_Tableaus/adjacency/Get_Adjacency.m
@@ -21,107 +21,172 @@ else
     
     flag_opers  = false;
     store_opers = [];
+    
 end
 
 n  = size(Tab,2);
 n  = (n-1)/2;
 Sx = Tab(:,1:n);
-Sz = Tab(:,n+1:2*n);
 
-if all(all(Sx==eye(n,'int8')))
+
+if all(diag(Sx)>0) %faster than all(all(Sx==eye(n,'int8'))) 
    
-    Gamma=Sz;
+    Gamma=Tab(:,n+1:2*n);
     
-    for ll=1:size(Gamma,1)
+    if ~flag_opers
+        
+        Gamma = Gamma - diag(diag(Gamma));
+        
+        
+    else
+        
+        for ll=1:size(Gamma,1)
 
-        if Gamma(ll,ll)==1 
-            %There is a Y operator, which can be removed with a phase gate.
-            %disp('There is a Y operator, I will <<perform>> a phase gate.')
+            if Gamma(ll,ll)==1 
+                %There is a Y operator, which can be removed with a phase gate.
+                %disp('There is a Y operator, I will <<perform>> a phase gate.')
 
-            if flag_opers
+                    cnt=cnt+1;
+                    store_opers(cnt).qubit=ll;
+                    store_opers(cnt).gate='P';
 
-                cnt=cnt+1;
-                store_opers(cnt).qubit=ll;
-                store_opers(cnt).gate='P';
+                    Gamma(ll,ll)=0;
 
             end
-
-            Gamma(ll,ll)=0;
-
-        end
+        end        
+        
     end
     
-    mustBeValidAdjacency(Gamma)
+    %mustBeValidAdjacency(Gamma)
     return
     
 end
 
 
-for qubit=1:n
-   
-    Tq    = Tab(:,qubit);
-    locs  = find(Tq(qubit:n));    %Check the columns to see if we have X
-    locs  = locs+qubit-1;
+if flag_opers
     
-    if isempty(locs)  %|| isempty(locs(locs>=qubit)) %If there is no X, do a Had
+    for qubit=1:n
 
-       %Tab=Had_Gate(Tab,qubit,n); %Do not call Had gate, we don't need the phase information
-       
-       temp           = Tab(:,qubit+n); 
-       Tab(:,qubit+n) = Tab(:,qubit);
-       Tab(:,qubit)   = temp;       
-    
-       if flag_opers
-           
+        Tq    = Tab(:,qubit);
+        locs  = Tq(qubit:end)>0;
+        %locs  = find(Tq(qubit:n));    %Check the columns to see if we have X
+        %locs  = locs+qubit-1;
+
+        if ~any(locs) %isempty(locs)  %|| isempty(locs(locs>=qubit)) %If there is no X, do a Had
+
+           %Tab=Had_Gate(Tab,qubit,n); %Do not call Had gate, we don't need the phase information
+
+           temp           = Tab(:,qubit+n); 
+           Tab(:,qubit+n) = Tab(:,qubit);
+           Tab(:,qubit)   = temp;       
+
            cnt=cnt+1;
-           store_opers(cnt).qubit=qubit;
+           store_opers(cnt).qubit=qubit;    
            store_opers(cnt).gate='H';
-           
-       end
-       
+
+        end
+
+
+        if Tab(qubit,qubit)~=1
+
+            SliceTab = Tab(:,qubit);
+
+            for jj=qubit+1:n
+
+                if SliceTab(jj)==1 %Tab(jj,qubit)==1  %SWAP rows
+
+                    temp         = Tab(jj,:);
+                    Tab(jj,:)    = Tab(qubit,:);
+                    Tab(qubit,:) = temp;
+
+                    break
+
+                end
+
+            end
+
+        end
+
+        SliceTab = Tab(:,qubit);
+
+        for jj=qubit+1:n %Remove Xs appearing below the diagonal entry
+
+            if SliceTab(jj)==1 %Tab(jj,qubit)==1
+
+                Tab(jj,:)=bitxor(Tab(jj,:),Tab(qubit,:));
+
+            end
+
+        end
+
     end
     
-    if Tab(qubit,qubit)~=1
+else
+    
+    for qubit=1:n
+
+        Tq    = Tab(:,qubit);
+        locs  = Tq(qubit:end)>0; %Check the columns to see if we have X
+        %locs  = find(Tq(qubit:n));    
+        %locs  = locs+qubit-1;
         
+        if ~any(locs) %isempty(locs)  || isempty(locs(locs>=qubit)) %If there is no X, do a Had
+
+           %Tab=Had_Gate(Tab,qubit,n); %Do not call Had gate, we don't need the phase information
+
+           temp           = Tab(:,qubit+n); 
+           Tab(:,qubit+n) = Tab(:,qubit);
+           Tab(:,qubit)   = temp;       
+
+        end
+
+
+        if Tab(qubit,qubit)~=1
+
+            SliceTab = Tab(:,qubit);
+
+            for jj=qubit+1:n
+
+                if SliceTab(jj)==1 %Tab(jj,qubit)==1  %SWAP rows
+
+                    temp         = Tab(jj,:);
+                    Tab(jj,:)    = Tab(qubit,:);
+                    Tab(qubit,:) = temp;
+
+                    break
+
+                end
+
+            end
+
+        end
+
         SliceTab = Tab(:,qubit);
         
-        for jj=qubit+1:n
+        for jj=qubit+1:n %Remove Xs appearing below the diagonal entry
 
-            if SliceTab(jj)==1 %Tab(jj,qubit)==1  %SWAP rows
+            if SliceTab(jj)>0 %Tab(jj,qubit)==1
 
-                temp         = Tab(jj,:);
-                Tab(jj,:)    = Tab(qubit,:);
-                Tab(qubit,:) = temp;
-
-                break
+                Tab(jj,:)=bitxor(Tab(jj,:),Tab(qubit,:));
 
             end
 
         end
         
-    end
-    
-    SliceTab = Tab(:,qubit);
-    
-    for jj=qubit+1:n %Remove Xs appearing below the diagonal entry
-       
-        if SliceTab(jj)==1 %Tab(jj,qubit)==1
-            
-            Tab(jj,:)=bitxor(Tab(jj,:),Tab(qubit,:));
-            
-        end
-        
+
     end
     
 end
 
-for qubit=1:n %Back-substitution
+%------------------ Back-substitution -------------------------------------
+
+for qubit=1:n 
     
    SliceTab = Tab(:,qubit); 
     
    for k=qubit-1:-1:1
        
-       if SliceTab(k)==1 %Tab(k,qubit)==1 
+       if SliceTab(k)>0 %Tab(k,qubit)==1 
            
            Tab(k,:)=bitxor(Tab(k,:),Tab(qubit,:));
            
@@ -165,14 +230,15 @@ else
     
 end
 
-Sx    = Tab(:,1:n);
+%This is for debugging.
+% Sx    = Tab(:,1:n);
+% 
+% if ~all(diag(Sx)>0) %~all(all(eye(n,'int8')==Sx))
+%     error('At the end of Gauss elimination, the Sx part of the Tableau is not identity')
+% end
 
-if ~all(all(eye(n,'int8')==Sx))
-    error('At the end of Gauss elimination, the Sx part of the Tableau is not identity')
-end
 
-
-mustBeValidAdjacency(Gamma)
+%mustBeValidAdjacency(Gamma)
 Gamma=single(Gamma);
 
 
