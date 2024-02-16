f1345ddf4eda9ca25e78fdd6ae0d4cc1d629c135
Eva Takou <etakou@vt.edu>
2024-02-15 23:28:03 -0500

    added scripts to post-process circuit

---
+++ Functionalities/Post_Process_Circuit/pass_X_gates.m
@@ -0,0 +1,54 @@
+function [Circuit]=pass_X_gates(Circuit,np)
+%Script to pass X gates after the emissions of photons.
+
+Gates  = flip(Circuit.Gate.name);
+qubits = flip(Circuit.Gate.qubit);
+   
+
+
+for photon=1:np
+    
+   
+    for k=1:length(Gates)
+        
+        %Check first gate that involves this photon:
+        if any(qubits{k}==photon) && strcmpi(Gates{k},'X')
+           
+            
+           %Mark this position:
+           Xpos = k;
+           
+           break
+            
+        end
+        
+        
+    end
+    
+    for k=Xpos+1:length(Gates)
+        
+        if any(qubits{k}==photon) && strcmpi(Gates{k},'CNOT')
+        
+            CNOT_pos=k;
+            Q = qubits{k};
+            break
+            
+        end
+        
+    end
+    
+    Gates        =[Gates(1:CNOT_pos),{'X'},Gates(CNOT_pos+1:end)];
+    qubits       =[qubits(1:CNOT_pos),{photon},qubits(CNOT_pos+1:end)];
+    Gates{Xpos}  =[];
+    qubits{Xpos} =[];
+    
+    
+end
+[Gates,qubits]=remove_empty_slots(Gates,qubits);
+Circuit.Gate.name=flip(Gates);
+Circuit.Gate.qubit=flip(qubits);
+
+
+
+
+end
---
+++ Functionalities/Post_Process_Circuit/remove_empty_slots.m
@@ -0,0 +1,24 @@
+function [Gates,Qubits]=remove_empty_slots(Gates,Qubits)
+%Function to remove the empty positions due to re-arrangement of gates.
+%Input: Gates: A cell array with the Clifford gate names e.g. 'CNOT','X','Z' etc
+%       Qubits: A cell array of qubits, ordered as Gates, on which we apply
+%       the gates.
+%Output: The Gates and Qubits cell after removing empty entries.
+
+to_remove=[];
+
+for kk=1:length(Gates)
+
+    if isempty(Gates{kk})
+
+       to_remove=[to_remove,kk];
+
+    end
+
+end
+
+Gates(to_remove)=[];
+Qubits(to_remove)=[];         
+
+
+end
