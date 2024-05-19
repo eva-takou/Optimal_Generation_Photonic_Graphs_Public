2ae18073d396f03db15a9c3f4bf301139e4552bc
Eva Takou <etakou@vt.edu>
2024-05-19 16:05:16 -0400

    cleanup

--- Functionalities/Create_graph_families/create_triangle_free_graph.m
+++ Functionalities/Create_graph_families/create_triangle_free_graph.m
@@ -1,5 +1,11 @@
 function Adj=create_triangle_free_graph(n)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
 
+%Input: n: # of nodes
+%Output: Adjacency matrix of triangle-free graph       
 
 while true
 
