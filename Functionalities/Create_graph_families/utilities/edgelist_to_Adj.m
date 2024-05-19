5c4cae14888da0767186ddf5797d20d653e44da7
Eva Takou <etakou@vt.edu>
2024-05-19 13:09:00 -0400

    cleanup

--- Functionalities/Create_graph_families/utilities/edgelist_to_Adj.m
+++ Functionalities/Create_graph_families/utilities/edgelist_to_Adj.m
@@ -1,4 +1,9 @@
 function Adj=edgelist_to_Adj(EdgeList,n)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
 %Function to convert an Edgelist to the adjacency matrix.
 %
 %Input: EdgeList: A cell array with the edges of the graph
@@ -9,13 +14,11 @@ Adj = sparse(n,n);
 
 for jj=1:length(EdgeList)
     
-   this_edge=EdgeList{jj} ;
+   this_edge = EdgeList{jj} ;
    
    Adj = Adj + sparse(this_edge(1),this_edge(2),1,n,n);
    Adj = Adj + sparse(this_edge(2),this_edge(1),1,n,n);
    
-    
 end
-
 
 end
