fbe391e972d16c6e22d26c37abeb17ddeef5e4d3
Eva Takou <etakou@vt.edu>
2024-05-19 13:03:41 -0400

    cleanup

--- Functionalities/Create_graph_families/create_Sn.m
+++ Functionalities/Create_graph_families/create_Sn.m
@@ -1,13 +1,19 @@
 function out=create_Sn(n,formatOption)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
 %Create a star graph of order n. 
 %Input: n: The number of nodes
 %       formatOption: 'Edgelist' or 'Adjacency' to control the output.
+%Output: Adjacency or Edgelist of star graph Sn
+
 
 cnt=0;
 
 central_node = 1; 
-
-other_nodes = setxor(1:n,central_node);
+other_nodes  = setxor(1:n,central_node);
 
 for kk=1:n-1
            
@@ -25,16 +31,12 @@ switch formatOption
         
         out = Edgelist;
         
-        
     case 'Adjacency'
 
 
         out = edgelist_to_Adj(Edgelist,n);
         
-        
-        
 end
-
 
 
 end
