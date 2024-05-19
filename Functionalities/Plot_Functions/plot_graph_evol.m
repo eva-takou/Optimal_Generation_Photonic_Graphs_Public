7a3f99ca8dae582c12bd39f216d4fdff64d03ca1
Eva Takou <etakou@vt.edu>
2024-05-19 12:50:03 -0400

    cleanup

--- Functionalities/Plot_Functions/plot_graph_evol.m
+++ Functionalities/Plot_Functions/plot_graph_evol.m
@@ -1,6 +1,21 @@
-function plot_graph_evol(graphs,emitters,fignum)
+function plot_graph_evol(graphs,emitters,fignum,emitter_Color)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
+%Input: graphs: a struct with fields 'Adjacency' and 'identifier'. The
+%               identifier describes the process that happened in the
+%               generation [e.g., time-reversed measurement (TRM), 
+%               disentanglement (DE), photon absorption (PA)].
+%       emitters: a vector (double) of node labels of emitters
+%       emitter_Color: the node color of emitters        
+%
+%By default, the node color of photons is black.
 
-figure(fignum)
+
+figure(fignum) 
+
 for jj=1:length(graphs.Adjacency)
    
     nexttile
@@ -9,15 +24,15 @@ for jj=1:length(graphs.Adjacency)
     h.NodeColor='k';
     h.EdgeAlpha=0.9;
     h.LineWidth=0.9;
-    h.NodeFontSize=10;
+    h.NodeFontSize=12;
     h.MarkerSize=6;
     set(gca,'xtick',[])
     set(gca,'ytick',[])
-    if jj>1
-        highlight(h, emitters, 'NodeColor', [0.4940 0.1840 0.5560]);
+    if jj>=1
+        highlight(h, emitters, 'NodeColor', emitter_Color);
     end
     
-    title(graphs.identifier{jj})
+    title(graphs.identifier{jj},'fontsize',14)
     
 end
 set(gcf,'color','w')
