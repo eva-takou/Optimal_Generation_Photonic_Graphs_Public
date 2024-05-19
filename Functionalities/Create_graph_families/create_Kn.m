f022fce41993ee3e91a5832adfc642da7940c107
Eva Takou <etakou@vt.edu>
2024-05-19 13:10:58 -0400

    cleanup

--- Functionalities/Create_graph_families/create_Kn.m
+++ Functionalities/Create_graph_families/create_Kn.m
@@ -1,27 +1,30 @@
 function out=create_Kn(n,formatOption)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
 %Create a complete graph of order n.
 %Input: n: # of qubits
 %       formatOption: 'Edgelist' or 'Adjacency' to control the output.
 %Output: The edgelist or adjacency of the graph.
 
 
+cnt = 0;
 
-cnt=0;
 for jj=1:n
     
     for kk=1:n
-        
     
         if kk>jj
            
             cnt=cnt+1;
             Edgelist{cnt}=[jj,kk];
             
-            
         end
         
-        
     end
+    
 end
 
 switch formatOption
@@ -30,16 +33,11 @@ switch formatOption
         
         out = Edgelist;
         
-        
     case 'Adjacency'
 
-
         out = edgelist_to_Adj(Edgelist,n);
-        
         out = (full(out));
         
 end
-
-
 
 end
