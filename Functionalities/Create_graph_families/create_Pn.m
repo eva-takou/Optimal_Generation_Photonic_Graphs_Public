650b9dd01504242a2b67902b417098d908554462
Eva Takou <etakou@vt.edu>
2024-05-19 13:10:11 -0400

    cleanup

--- Functionalities/Create_graph_families/create_Pn.m
+++ Functionalities/Create_graph_families/create_Pn.m
@@ -1,23 +1,34 @@
 function out=create_Pn(n,formatOption)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
 %Create a path graph of order n.
 %Input: n: # of qubits
 %       formatOption: 'Edgelist' or 'Adjacency' to control the output.
 %Output: The edgelist or adjacency of the graph.
 
-cnt=0;
+cnt = 0;
+
 for jj=1:n
     
     for kk=jj+1:n %only egdes between v_{i+1}v_{i}
         
     
         if kk-jj==1
-            cnt=cnt+1;
-            EdgeList{cnt}=[jj,kk];
+            
+            cnt           = cnt+1;
+            EdgeList{cnt} = [jj,kk];
+            
         else
+            
             break
+            
         end
         
     end
+    
 end
 
 switch formatOption
@@ -26,16 +37,11 @@ switch formatOption
         
         out = EdgeList;
         
-        
     case 'Adjacency'
 
-
         out = edgelist_to_Adj(EdgeList,n);
-        
         out = full(out);
         
 end
-
-
 
 end
