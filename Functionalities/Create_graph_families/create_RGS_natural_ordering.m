ee3e5f1194504a0c48022a9ea9d8dd7c2c194a8b
Eva Takou <etakou@vt.edu>
2024-05-19 13:07:01 -0400

    cleanup

--- Functionalities/Create_graph_families/create_RGS_natural_ordering.m
+++ Functionalities/Create_graph_families/create_RGS_natural_ordering.m
@@ -1,13 +1,18 @@
 function Adj=create_RGS_natural_ordering(n)
-%n: number of core qubits
-%Adj: adjacency matrix of RGS with natural emission ordering.
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
+%Input: n: number of core qubits
+%Output: Adjacency matrix of RGS with natural emission ordering.
 
-cores  = 1:2:2*n;
-leaves = 2:2:2*n;
-EdgeList={};
+cores    = 1:2:2*n;
+leaves   = 2:2:2*n;
+EdgeList = {};
+
 for l1=1:length(cores)
    
-    
     for l2=l1+1:length(cores)
         EdgeList=[EdgeList,{[cores(l1),cores(l2)]}];
     end
