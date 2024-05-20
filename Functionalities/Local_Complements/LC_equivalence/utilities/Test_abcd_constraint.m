5fb612b809c355f46d6f9a3cbe46bb76a13d9122
Eva Takou <etakou@vt.edu>
2024-05-19 21:28:03 -0400

    updates

--- Functionalities/Local_Complements/LC_equivalence/utilities/Test_abcd_constraint.m
+++ Functionalities/Local_Complements/LC_equivalence/utilities/Test_abcd_constraint.m
@@ -1,34 +1,20 @@
 function cond = Test_abcd_constraint(NullVec,n)
-%Input: NullVec: A single null-vector 4x x 1.
-%       n: 1/4 of the total unknowns a's, b's, c's, d's.
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
+%Input:  NullVec: A single null-vector 4x x 1.
+%        n: 1/4 of the total unknowns a's, b's, c's, d's.
+%Output: True or False (whether or not the NullVector satisfies the
+%        constraint det(Qi)=1).
 
 [a_s,b_s,c_s,d_s] = Get_abcd_coeffs(NullVec,n);
-
-%cnt=0;
-
-% for ii=1:n
-% 
-%     if mod(a_s(ii)*d_s(ii)+b_s(ii)*c_s(ii),2)==1
-% 
-%         cnt=cnt+1;
-% 
-%     end
-% 
-% 
-% end
-
-% if cnt==n
-% 
-%    cond=true;
-% else
-%     cond=false;
-% end
 
 if all(mod(a_s.*d_s+b_s.*c_s,2)==1)
     cond=true;
 else
     cond=false;
 end
-
 
 end
