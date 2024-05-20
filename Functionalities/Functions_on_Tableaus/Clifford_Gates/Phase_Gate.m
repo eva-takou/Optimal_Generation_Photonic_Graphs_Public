93023c2ec64f66e9a8c952ccca3d68b7ae74972f
Eva Takou <etakou@vt.edu>
2024-05-20 00:11:23 -0400

    updates

--- Functionalities/Functions_on_Tableaus/Clifford_Gates/Phase_Gate.m
+++ Functionalities/Functions_on_Tableaus/Clifford_Gates/Phase_Gate.m
@@ -1,8 +1,12 @@
 function Tab=Phase_Gate(Tab,qubit,n)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
 %Implementation of the phase gate on the stabilizer Tableau.
-%Inputs: Tab: The Tableau
-%        qubit: an array of 2 qubits (first one is control)
-%        n: the # of qubits represented by the Tableau.
+%Inputs: Tab: The stabilizer Tableau (n x 2n+1 array)
+%        qubit: index of qubit as represented in the Tableau
+%        n: the # of qubits represented by the Tableau
 %Output: The updated Tableau.
 
 X = Tab(:,qubit);
