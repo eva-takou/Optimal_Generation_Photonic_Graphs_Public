c7de9ed7cab9a2efd01064ce7524cc0ef7228cd9
Eva Takou <etakou@vt.edu>
2024-05-20 00:10:41 -0400

    updates

--- Functionalities/Functions_on_Tableaus/Clifford_Gates/Pauli_Gate.m
+++ Functionalities/Functions_on_Tableaus/Clifford_Gates/Pauli_Gate.m
@@ -1,40 +1,35 @@
 function Tab=Pauli_Gate(Tab,qubit,n,Pauli)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
 %Implementation of the Pauli X,Y, or Z gate on the stabilizer Tableau.
-%Inputs: Tab: The Tableau
-%        qubit: an array of 2 qubits (first one is control)
-%        n: the # of qubits represented by the Tableau.
-%        Pauli: 'X', 'Y', or 'Z'
+%Inputs: Tab: The stabilizer Tableau (n x 2n+1 array)
+%        qubit: index of qubit as represented in the Tableau
+%        n: the # of qubits represented by the Tableau
+%        Pauli: 'X', 'Y', or 'Z' (char)
 %Output: The updated Tableau.
-
-%Update from this: https://arxiv.org/pdf/2311.03906.pdf
+%
+%Based on Ref: https://arxiv.org/pdf/2311.03906.pdf
 
 switch Pauli
     
-    case 'X' %X gate on qubit a: ria=ria+zia (stabs and destabs)
+    case 'X' %X gate on qubit a: ri_a=ri_a+zi_a 
 
-        %Find where we have non-zero zia:
-        zi             = Tab(:,qubit+n);
+        zi         = Tab(:,qubit+n);
         Tab(:,end) = bitxor(Tab(:,end),zi);
         
-
+    case 'Y' %Y gate on qubit a: ri_a=ri_a+zi_a+xi_a 
         
-    case 'Y' %Y gate on qubit a: ria=ria+zia+xia (stabs and destabs)
-        
-        zi             = Tab(:,qubit+n);
-        xi             = Tab(:,qubit);
+        zi         = Tab(:,qubit+n);
+        xi         = Tab(:,qubit);
         Tab(:,end) = bitxor(Tab(:,end),bitxor(zi,xi));
         
-      
+    case 'Z' %Y gate on qubit a: ri_a=ri_a+x_ia 
         
-    case 'Z' %Y gate on qubit a: ria=ria+xia (stabs and destabs)
-        
-        
-        xi             = Tab(:,qubit);
+        xi         = Tab(:,qubit);
         Tab(:,end) = bitxor(Tab(:,end),xi);
-        
-      
         
 end
        
-
 end
