8c149e2e5207eb9ebe7af7f65b0a144030ad0850
Eva Takou <etakou@vt.edu>
2024-05-20 00:24:44 -0400

    updates

--- Functionalities/Validations/isbinary.m
+++ Functionalities/Validations/isbinary.m
@@ -1,7 +1,12 @@
 function flag=isbinary(A)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
+%Test if the input matrix is binary.
+%Input:  A: Matrix
+%Output: flag: true or false
 
 flag = all( all(A==0 | A==1) );
-
-
 
 end
--- Functionalities/Validations/mustBeAbelian.m
+++ Functionalities/Validations/mustBeAbelian.m
@@ -1,24 +1,29 @@
 function mustBeAbelian(S)
-%Input should be a stabilizer array of dimension n x 2*n. (SX|SZ)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
+%Test if the input stabilizer array is abelian (commutative).
+%
+%Input: S: Stabilizer array nx2n (SX|SZ) (w/o the phases)
 
 [n,N] = size(S);
 
 if N~=2*n
-   error('The input array in mustBeAbelian is not n x 2n.') 
+   
+    error('The input array in mustBeAbelian is not n x 2n.') 
+    
 end
 
 PZ2 = [zeros(n,n,'int8') , eye(n,'int8') ; ...
        eye(n,'int8')    , zeros(n,n,'int8') ];
-   
 
 if any(mod(double(S)*double(PZ2)*double(S.'),2)~=zeros(n,n,'int8'))
     
-     ME=MException('mustBeAbelian:inputError','Input set is not Abelian');
+     ME = MException('mustBeAbelian:inputError','Input set is not Abelian');
     
      throw(ME)
     
 end
-
-
 
 end
