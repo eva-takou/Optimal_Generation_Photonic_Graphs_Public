164d076f28f01b35144c4051ec05e4f1184144c3
Eva Takou <etakou@vt.edu>
2024-05-20 00:07:08 -0400

    updates

--- Functionalities/Functions_on_Tableaus/Clifford_Gates/Had_Gate.m
+++ Functionalities/Functions_on_Tableaus/Clifford_Gates/Had_Gate.m
@@ -1,33 +1,20 @@
 function Tab=Had_Gate(Tab,qubit,n)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
+%
 %Implementation of the Hadamard gate on the stabilizer Tableau.
-%Inputs: Tab: The Tableau
-%        qubit: an array of 2 qubits (first one is control)
+%Inputs: Tab: The stabilizer Tableau (n x 2n+1 array)
+%        qubit: index of qubit as represented in the Tableau
 %        n: the # of qubits represented by the Tableau.
 %Output: The updated Tableau.
-
 
 XZ         = Tab(:,qubit).*Tab(:,qubit+n);
 Tab(:,end) = bitxor(XZ,Tab(:,end));         
 
-% locsX = Tab(:,qubit)==1;
-% locsZ = Tab(:,qubit+n)==1;
-% locsY = XZ==1; %we know these from multiplication XZ
-% locsX = locsX & ~locsY;
-% locsZ = locsZ & ~locsY;
-
-%testTab=Tab;
-% Tab(locsX,qubit)=0;
-% Tab(locsX,qubit+n)=1;
-% Tab(locsZ,qubit+n)=0;
-% Tab(locsZ,qubit)=1;
-
-
-%Tab(:,[qubit,qubit+n]) = Tab(:,[qubit+n,qubit]); %This is slow...
-
-temp = Tab(:,qubit+n); %For some reason this is better..
-Tab(:,qubit+n)=Tab(:,qubit);
-Tab(:,qubit)=temp;
-
-
+temp           = Tab(:,qubit+n); 
+Tab(:,qubit+n) = Tab(:,qubit);
+Tab(:,qubit)   = temp;
 
 end
