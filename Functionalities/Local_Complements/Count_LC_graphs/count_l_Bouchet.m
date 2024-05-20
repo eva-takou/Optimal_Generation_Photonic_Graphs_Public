5c3eb2a1001891ab730fb684d4b9a770643fe465
Eva Takou <etakou@vt.edu>
2024-05-19 22:27:04 -0400

    updates

--- Functionalities/Local_Complements/Count_LC_graphs/count_l_Bouchet.m
+++ Functionalities/Local_Complements/Count_LC_graphs/count_l_Bouchet.m
@@ -1,6 +1,13 @@
 function l=count_l_Bouchet(Adj)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
+%
 %Get the # of LC graphs of an input graph. The input graph has to be a
 %circle graph to be able to extract the size of the LC orbit.
+%Based on Bouchet's paper: Recognizing locally equivalent graphs, 1991.
+%
 %Input: Adj: Adjacency matrix
 %Ouput: l: size of LC orbit
 
@@ -9,7 +16,7 @@ function l=count_l_Bouchet(Adj)
 if ~bool
     
     warning('Cannot count LC graphs if the input is not an alternance graph.')
-    l=[];
+    l=nan;
     return
     
 end
