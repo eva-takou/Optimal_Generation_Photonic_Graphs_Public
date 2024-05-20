3747bca19430f0afc66691986853584d55e94b3a
Eva Takou <etakou@vt.edu>
2024-05-20 00:26:46 -0400

    updates

--- Functionalities/Validations/mustBeBinary.m
+++ Functionalities/Validations/mustBeBinary.m
@@ -1,14 +1,17 @@
 function mustBeBinary(A)
-
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
+%Throw an error if the input matrix is not binary.
+%
+%Input: A: Matrix
 
 if ~isbinary(A)
     
-     ME=MException('mustBeBinary:inputError','Input must be binary.');
-    
+     ME = MException('mustBeBinary:inputError','Input must be binary.');
      throw(ME)
     
 end
-
-
 
 end
--- Functionalities/Validations/mustBeSimple.m
+++ Functionalities/Validations/mustBeSimple.m
@@ -1,14 +1,17 @@
 function mustBeSimple(A)
-
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 20, 2024
+%
+%Throw an error if the adjacency matrix is not simple
+%Input: A: adjacency matrix
 
 if any(diag(A))
     
-     ME=MException('mustBeSimple:inputError','Detected self loop in adjacency matrix.');
-    
+     ME = MException('mustBeSimple:inputError','Detected self loop in adjacency matrix.');
      throw(ME)
     
 end
-
 
 
 end
