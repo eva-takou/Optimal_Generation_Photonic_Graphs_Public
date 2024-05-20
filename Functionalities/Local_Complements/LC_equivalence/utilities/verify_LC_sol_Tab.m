a2b1021525ea5a58a2d6b9a6e7b5eb84bc514a31
Eva Takou <etakou@vt.edu>
2024-05-19 21:33:37 -0400

    updates

--- Functionalities/Local_Complements/LC_equivalence/utilities/verify_LC_sol_Tab.m
+++ Functionalities/Local_Complements/LC_equivalence/utilities/verify_LC_sol_Tab.m
@@ -1,27 +1,30 @@
 function [str_sols]=verify_LC_sol_Tab(G1,G2,str_sols)
-%Function to verify the LC test, by inspecting if we generate the same
-%graph.
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
 %
-%Sometimes, due to having to apply again local gates, we might not recover
-%exactly the same matrix.
+%Function to verify the LC test, by inspecting if we generate the same graph.
+%Sometimes, due to having to apply again local gates to put in canonical form, 
+%we might not recover exactly the same matrix.
 %
-%
-close all;
-n=length(G1);
+%Input: G1: Adjacency matrix of 1st graph
+%       G2: Adjacency matrix of 2nd graph  
+%       str_sols: The solutions as a string of operations for the LC equivalence
 
-conds=false(1,length(str_sols));
+n     = length(G1);
+conds = false(1,length(str_sols));
 
 for jj=1:length(str_sols)
     
-    temp2=Tableau_Class(G2,'Adjacency');
-    
+    temp2    = Tableau_Class(G2,'Adjacency');
     this_sol = str_sols{jj}; %A list of operations to apply
    
     %could be 1, H, P, HP, PH, HPH.
    
     for k=1:n
 
-       oper=this_sol{k};
+       oper = this_sol{k};
 
        if strcmpi(oper,'H') || strcmpi(oper,'P')
 
@@ -42,48 +45,42 @@ for jj=1:length(str_sols)
            temp2 = temp2.Apply_Clifford(k,'H',n);
            temp2 = temp2.Apply_Clifford(k,'P',n);
            temp2 = temp2.Apply_Clifford(k,'H',n);
-       %or 1, so we apply nothing.
+           %or 1, so we apply nothing.
 
        end
 
-
     end
     
-    G2_test=Get_Adjacency(temp2.Tableau);
+    G2_test = Get_Adjacency(temp2.Tableau);
     
     if all(all(G2_test==G1))
        
-        conds(jj)=true;
+        conds(jj) = true;
+        
     else
-        conds(jj)=false;
+        
+        conds(jj) = false;
         
     end
     
-    
 end
-
-% figure(1)
-% subplot(3,1,1)
-% plot(graph(G1))
-% subplot(3,1,2)
-% plot(graph(G2))
-% subplot(3,1,3)
-% plot(graph(G2_test))
 
 if ~any(conds)
     
    %Try the sequence in reverse:
     for jj=1:length(str_sols)
 
-        temp2=Tableau_Class(G2,'Adjacency');
-
+        temp2    = Tableau_Class(G2,'Adjacency');
         this_sol = str_sols{jj}; %A list of operations to apply
         
         for k=1:n
            
             if strcmpi(this_sol{k},'HP')
+                
                 this_sol{k}='PH';
+                
             elseif strcmpi(this_sol{k},'PH')
+                
                 this_sol{k}='HP';
                 
             end
