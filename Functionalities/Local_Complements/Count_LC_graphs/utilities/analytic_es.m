5df240c84ea992c9ad23c3741d073b580a1d61a7
Eva Takou <etakou@vt.edu>
2024-05-19 22:34:56 -0400

    updates

--- Functionalities/Local_Complements/Count_LC_graphs/utilities/analytic_es.m
+++ Functionalities/Local_Complements/Count_LC_graphs/utilities/analytic_es.m
@@ -1,5 +1,13 @@
 function e=analytic_es(n,graphtype)
-
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%
+%Get the index e(G) for various graph types.
+%
+%Input: n: # of graph nodes
+%      graphtype: can be: 'Pn', 'Kn','Sn', 'RGS_n_n', 'Cn'
+%Output: The value of e(G) for the closed-form expression.
 
 
 switch graphtype
@@ -7,92 +15,32 @@ switch graphtype
     
     case 'Pn' %OK
         
-        if n==1
-
-            e=2;
-        
-        elseif n==2
-            
-            e = 6;
-            
-        elseif n>2
-            
-            e = sqrt(3)/6*( (1+sqrt(3))^(n+1)-(1-sqrt(3))^(n+1) );
-            
-            
-        end
-        
-        
+        e = sqrt(3)/6*( (1+sqrt(3))^(n+1)-(1-sqrt(3))^(n+1) );
         
     case 'Sn' %OK
         
-        if n==2
-            
-            e=6;
-            
-        else
-            
-            e = 2^(n-1)*(n+1);
-            
-        end
-            
+        e = 2^(n-1)*(n+1);
         
         
     case 'Kn'%OK
         
         e = 2^(n-1)*(n+1);
-        
-    case 'RGS_s' %OK -- is this global?
-        
-        if n>=3
-            
-            e=2^(n-2)*(3*n-1);
-            
-        end
-        
-    case 'RGS_d' %OK
-        
-        e = 2^((n-2)-1)*(9*(n-2)+3);
-        
-        
-        
-    case 'RGS_n_n_minus_1' %NOT OK
-        
-        
-        e = 2^(n-(n+1)/2)*(26*(n-(n+1)/2)-22);
-        
-    case 'RGS_n_n' %NOT OK
-        
+         
+    case 'RGS_n_n' %OK
         
         e = 2^(n-1)+6^(n-1)*(3+2*n);
         
-        
-        
     case 'Cn'
         
-        if n>=5 %OK
+        if n>3 %OK
             
             e = (1+sqrt(3))^n+(1-sqrt(3))^n-4/3*(2^(n-1)+(-1)^n) ;
             
-        elseif n==4 %this is?
+        else
             
-            e=44;
-            
-        elseif n==3 %%OK
-            
-            
-            e = 2^(n-1)*(n+1);
-            
-            
-            
-        elseif n==2 %becomes path graph
-            
-            e=6;
+            error('The Cn graph is defined for n>=3.')
            
-            
         end
-        
-        
         
 end
 
