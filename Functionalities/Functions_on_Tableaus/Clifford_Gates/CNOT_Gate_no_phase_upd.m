2fc53334ec65f3c8399bc7ecd9713c4c714f2102
Eva Takou <etakou@vt.edu>
2024-05-20 00:05:48 -0400

    updates and script for CNOT w/o phase update

--- Functionalities/Functions_on_Tableaus/Clifford_Gates/CNOT_Gate_no_phase_upd.m
+++ Functionalities/Functions_on_Tableaus/Clifford_Gates/CNOT_Gate_no_phase_upd.m
@@ -1,9 +1,10 @@
-function  Tab=CNOT_Gate(Tab,qubit,n)
+function  Tab=CNOT_Gate_no_phase_upd(Tab,qubit,n)
 %--------------------------------------------------------------------------
 %Created by Eva Takou
 %Last modified: May 19, 2024
 %
-%Implementation of the CNOT gate on the stabilizer Tableau. 
+%Implementation of the CNOT gate on the stabilizer Tableau w/o the phase
+%update. 
 %Inputs: Tab: The stabilizer Tableau (n x 2n+1 array)
 %        qubit: an array of 2 qubits (first one is control)
 %        n: the # of qubits represented by the Tableau
@@ -11,22 +12,11 @@ function  Tab=CNOT_Gate(Tab,qubit,n)
 
 XC     = Tab(:,qubit(1));
 ZC     = Tab(:,qubit(1)+n);
-r      = Tab(:,end);
 
 XT     = Tab(:,qubit(2));
 ZT     = Tab(:,qubit(2)+n);
 
 Tab(:,qubit(2))    = bitxor(XT,XC);
 Tab(:,qubit(1)+n)  = bitxor(ZT,ZC);
-
-%The phase update is: XC.*ZT.*( bitxor( XT ,bitxor(ZC,1)) );
-temp = mod(ZC+1,2);     %Mod is faster here than bitxor
-temp = bitxor(XT,temp);
-temp = ZT.*temp;
-temp = XC.*temp;
-
-
-Tab(:,end) = bitxor(temp,r); 
-
 
 end
