b9e18ac32892dc02793d96dd670c2576a473c5b0
Eva Takou <etakou@vt.edu>
2024-05-20 00:29:36 -0400

    updates

--- Functionalities/Validations/mustBeValidAdjacency.m
+++ Functionalities/Validations/mustBeValidAdjacency.m
@@ -1,22 +1,24 @@
 function mustBeValidAdjacency(A)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 20, 2024
+%
+%Throw an error if the adjacency matrix is not valid
+%Input: A: The adjacency matrix
 
 mustBeSimple(A)
 
-
 if ~issymmetric(single(A))
 
-ME=MException('mustBeValidAdjacency:inputError','Detected non-symmetric Adjacency matrix.');
-    
-throw(ME)
+    ME = MException('mustBeValidAdjacency:inputError','Detected non-symmetric Adjacency matrix.');
+    throw(ME)
 
 end
 
-
 if size(A,1)~=size(A,2)
     
-ME=MException('mustBeValidAdjacency:inputError','Adjacency matrix is not square.');
-    
-throw(ME)
+    ME = MException('mustBeValidAdjacency:inputError','Adjacency matrix is not square.');
+    throw(ME)
     
 end
 
