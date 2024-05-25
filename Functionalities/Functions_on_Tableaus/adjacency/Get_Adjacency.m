8a570b47a40e70749379e46a43e34b3c6e829faa
Eva Takou <etakou@vt.edu>
2024-05-25 11:21:27 -0400

    updates

--- Functionalities/Functions_on_Tableaus/adjacency/Get_Adjacency.m
+++ Functionalities/Functions_on_Tableaus/adjacency/Get_Adjacency.m
@@ -1,61 +1,24 @@
-function [Gamma,store_opers]=Get_Adjacency(varargin) %(Tab)
+function [Gamma]=Get_Adjacency(Tab) 
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 25, 2024
+%--------------------------------------------------------------------------
+%
 %Function to obtain the adjacency matrix from a Stabilizer Tableau.
-%Input:  Tableau and 
+%Input:  Tab: Input stabilizer tableau (n x 2n+1 array)
 %        option: true or false, to store or not the operations that transform
 %        the Tableau in canonical form.
 %Output: Gamma: Adjacency matrix
-%        store_opers: The operations that transform the Sz part of the
-%        tableau to the canonical form.
 
-%To speed up the computation, discard the phase information.
-%Instead of rowsum do bitxor_rows
-
-Tab = varargin{1};
-cnt = 0;
-
-if nargin>1
-    
-    flag_opers = true;
-    
-else
-    
-    flag_opers  = false;
-    store_opers = [];
-    
-end
-
-n  = size(Tab,2);
-n  = (n-1)/2;
+Tab(:,end)=[]; %Drop the phase info.
+n  = size(Tab,1);
 Sx = Tab(:,1:n);
 
 
 if all(diag(Sx)>0) %faster than all(all(Sx==eye(n,'int8'))) 
    
-    Gamma=Tab(:,n+1:2*n);
-    
-    if ~flag_opers
-        
-        Gamma = Gamma - diag(diag(Gamma));
-        
-        
-    else
-        
-        for ll=1:size(Gamma,1)
-
-            if Gamma(ll,ll)==1 
-                %There is a Y operator, which can be removed with a phase gate.
-                %disp('There is a Y operator, I will <<perform>> a phase gate.')
-
-                    cnt=cnt+1;
-                    store_opers(cnt).qubit=ll;
-                    store_opers(cnt).gate='P';
-
-                    Gamma(ll,ll)=0;
-
-            end
-        end        
-        
-    end
+    Gamma = Tab(:,n+1:end);
+    Gamma = Gamma - diag(diag(Gamma));
     
     %mustBeValidAdjacency(Gamma)
     return
@@ -63,183 +26,158 @@ if all(diag(Sx)>0) %faster than all(all(Sx==eye(n,'int8')))
 end
 
 
-if flag_opers
-    
-    for qubit=1:n
+for qubit=1:n
 
-        Tq    = Tab(:,qubit);
-        locs  = Tq(qubit:end)>0;
-        %locs  = find(Tq(qubit:n));    %Check the columns to see if we have X
-        %locs  = locs+qubit-1;
+    Tq    = Tab(:,qubit);     %Check the columns to see if we have X
+    locs  = Tq>0;
+    %locs  = Tq(qubit:end)>0;   %Alternatively
 
-        if ~any(locs) %isempty(locs)  %|| isempty(locs(locs>=qubit)) %If there is no X, do a Had
+    if ~any(locs) %isempty(locs)  || isempty(locs(locs>=qubit)) %If there is no X, do a Had
 
-           %Tab=Had_Gate(Tab,qubit,n); %Do not call Had gate, we don't need the phase information
+       %Apply Hadamard:
+       temp           = Tab(:,qubit+n); 
+       Tab(:,qubit+n) = Tab(:,qubit);
+       Tab(:,qubit)   = temp;    
 
-           temp           = Tab(:,qubit+n); 
-           Tab(:,qubit+n) = Tab(:,qubit);
-           Tab(:,qubit)   = temp;       
+    else
 
-           cnt=cnt+1;
-           store_opers(cnt).qubit=qubit;    
-           store_opers(cnt).gate='H';
+        apply_Had=true;
 
-        end
+        for l=qubit:n
 
+            if Tq(l)>0
 
-        if Tab(qubit,qubit)~=1
+                apply_Had=false;                 
 
-            SliceTab = Tab(:,qubit);
-
-            for jj=qubit+1:n
-
-                if SliceTab(jj)==1 %Tab(jj,qubit)==1  %SWAP rows
-
-                    temp         = Tab(jj,:);
-                    Tab(jj,:)    = Tab(qubit,:);
-                    Tab(qubit,:) = temp;
-
-                    break
-
-                end
+                break
 
             end
 
         end
 
+        if apply_Had
+
+           temp           = Tab(:,qubit+n); 
+           Tab(:,qubit+n) = Tab(:,qubit);
+           Tab(:,qubit)   = temp;  
+
+        end
+
+    end
+
+
+    if Tab(qubit,qubit)~=1
+
         SliceTab = Tab(:,qubit);
 
-        for jj=qubit+1:n %Remove Xs appearing below the diagonal entry
+        for jj=qubit+1:n
 
-            if SliceTab(jj)==1 %Tab(jj,qubit)==1
+            if SliceTab(jj)==1 %Tab(jj,qubit)==1  %SWAP rows
 
-                Tab(jj,:)=bitxor(Tab(jj,:),Tab(qubit,:));
+                temp         = Tab(jj,:);
+                Tab(jj,:)    = Tab(qubit,:);
+                Tab(qubit,:) = temp;
+
+                break
 
             end
 
         end
 
     end
-    
-else
-    
-    for qubit=1:n
 
-        Tq    = Tab(:,qubit);
-        locs  = Tq(qubit:end)>0; %Check the columns to see if we have X
-        %locs  = find(Tq(qubit:n));    
-        %locs  = locs+qubit-1;
-        
-        if ~any(locs) %isempty(locs)  || isempty(locs(locs>=qubit)) %If there is no X, do a Had
+    SliceTab    = Tab(:,qubit);
+    rowToBitXor = Tab(qubit,:); 
+    indx        = find(SliceTab(qubit+1:end))+qubit;
 
-           %Tab=Had_Gate(Tab,qubit,n); %Do not call Had gate, we don't need the phase information
+    for k=1:length(indx)
 
-           temp           = Tab(:,qubit+n); 
-           Tab(:,qubit+n) = Tab(:,qubit);
-           Tab(:,qubit)   = temp;       
-
-        end
-
-
-        if Tab(qubit,qubit)~=1
-
-            SliceTab = Tab(:,qubit);
-
-            for jj=qubit+1:n
-
-                if SliceTab(jj)==1 %Tab(jj,qubit)==1  %SWAP rows
-
-                    temp         = Tab(jj,:);
-                    Tab(jj,:)    = Tab(qubit,:);
-                    Tab(qubit,:) = temp;
-
-                    break
-
-                end
-
-            end
-
-        end
-
-        SliceTab = Tab(:,qubit);
-        
-        for jj=qubit+1:n %Remove Xs appearing below the diagonal entry
-
-            if SliceTab(jj)>0 %Tab(jj,qubit)==1
-
-                Tab(jj,:)=bitxor(Tab(jj,:),Tab(qubit,:));
-
-            end
-
-        end
-        
+        Tab(indx(k),:)=bitxor(Tab(indx(k),:),rowToBitXor);
 
     end
-    
+
+
+%         for jj=qubit+1:n %Remove Xs appearing below the diagonal entry
+% 
+%             if SliceTab(jj)>0 %Tab(jj,qubit)==1
+% 
+%                 Tab(jj,:)=bitxor(Tab(jj,:),rowToBitXor); %0.266 sec
+% 
+%             end
+% 
+%         end                
+
+
 end
 
-%------------------ Back-substitution -------------------------------------
 
-for qubit=1:n 
-    
-   SliceTab = Tab(:,qubit); 
-    
-   for k=qubit-1:-1:1
-       
-       if SliceTab(k)>0 %Tab(k,qubit)==1 
-           
-           Tab(k,:)=bitxor(Tab(k,:),Tab(qubit,:));
-           
-       end
-       
-   end
+
+%---Check alternative back-substitution -----------------------------------
+
+Tab=Tab'; %Transpose to access column-wise later
+
+for qubit=n:-1:2
    
+    SliceTab = Tab(qubit,:);
+    rowToBitXor = Tab(:,qubit);
+    
+    indx = find(SliceTab(1:qubit-1));
+    
+   for k=length(indx):-1:1
+      
+       Tab(:,indx(k))=bitxor(Tab(:,indx(k)),rowToBitXor);
+       
+   end    
+    
 end
+
+Tab=Tab'; %Revert back
+
+%-------- (Alternative Back-substitution w/o transposition) ---------------
+
+
+% for qubit=n:-1:2
+%     
+%    SliceTab    = Tab(:,qubit); 
+%    locs        = SliceTab>0;
+%    rowToBitXor = Tab(qubit,:);
+%    
+%    indx = find(SliceTab(1:qubit-1));
+%    
+%    for k=length(indx):-1:1
+%       
+%        Tab(indx(k),:)=bitxor(Tab(indx(k),:),rowToBitXor);
+%        
+%    end
+%    
+%    for k=qubit-1:-1:1
+%        
+%        if locs(k) %SliceTab(k)>0 %Tab(k,qubit)==1 
+%            
+%            Tab(k,:)=bitxor(Tab(k,:),rowToBitXor);
+%            
+%        end
+%        
+%    end
+%    
+% end
+%--------------------------------------------------------------------------
 
 %------------------ Remove self-loops -------------------------------------
 
-if flag_opers
-   
-    for ll=1:n
-        
-        if Tab(ll,ll+n)==1 %Eliminate Ys (remove self-loops)
+Gamma = Tab(:,n+1:2*n);
+Gamma = Gamma - diag(diag(Gamma));
 
-            %Tab = Clifford_Gate(Tab,ll,'P',n); 
-            %Tab=Phase_Gate(Tab,ll,n);
-
-            %Tab(:,ll+n) = bitxor(Tab(:,ll),Tab(:,ll+n));                    
-
-            Tab(ll,ll+n)=0;
-
-            cnt=cnt+1;
-            store_opers(cnt).qubit=ll;
-            store_opers(cnt).gate='P';
-
-        end
-        
-        
-        
-    end
-    
-    Gamma = Tab(:,n+1:2*n);
-    
-else
-    
-    Gamma = Tab(:,n+1:2*n);
-    Gamma = Gamma - diag(diag(Gamma));
-    
-end
-
-%This is for debugging.
+%---------- Uncomment for error-check: ------------------------------------
 % Sx    = Tab(:,1:n);
 % 
 % if ~all(diag(Sx)>0) %~all(all(eye(n,'int8')==Sx))
 %     error('At the end of Gauss elimination, the Sx part of the Tableau is not identity')
 % end
-
-
+%
+%
 %mustBeValidAdjacency(Gamma)
+%--------------------------------------------------------------------------
 Gamma=single(Gamma);
-
 
 end
