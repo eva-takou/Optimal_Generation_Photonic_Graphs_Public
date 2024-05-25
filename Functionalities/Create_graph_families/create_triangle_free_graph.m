ffd054deb0a6a4738a41322279c763e4c33951aa
Eva Takou <etakou@vt.edu>
2024-05-25 08:26:09 -0400

    updates

--- Functionalities/Create_graph_families/create_triangle_free_graph.m
+++ Functionalities/Create_graph_families/create_triangle_free_graph.m
@@ -10,20 +10,13 @@ function Adj=create_triangle_free_graph(n)
 while true
 
     Adj = create_random_graph(n);
-    Adj = double(Adj);
-    if trace(Adj*Adj*Adj)==0
+    
+    if trace(single(Adj)*single(Adj)*single(Adj))==0
        
         break
         
     end
     
 end
-
-
-
-
-
-
-
 
 end
