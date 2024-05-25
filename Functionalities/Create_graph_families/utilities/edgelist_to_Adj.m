fcd8bce794d281ecb6a50ade7eb7817bc73e58e4
Eva Takou <etakou@vt.edu>
2024-05-25 08:23:36 -0400

    sparse -> int8 update

--- Functionalities/Create_graph_families/utilities/edgelist_to_Adj.m
+++ Functionalities/Create_graph_families/utilities/edgelist_to_Adj.m
@@ -8,17 +8,19 @@ function Adj=edgelist_to_Adj(EdgeList,n)
 %
 %Input: EdgeList: A cell array with the edges of the graph
 %       n: # of graph nodes
-%Output: Adj: The adjacency matrix of the graph (sparse)
+%Output: Adj: The adjacency matrix of the graph (int8)
 
-Adj = sparse(n,n);
+Adj = zeros(n,n,'int8');
 
 for jj=1:length(EdgeList)
     
    this_edge = EdgeList{jj} ;
-   
-   Adj = Adj + sparse(this_edge(1),this_edge(2),1,n,n);
-   Adj = Adj + sparse(this_edge(2),this_edge(1),1,n,n);
+   v         = this_edge(1);
+   w         = this_edge(2);
+   Adj(v,w)  = 1;
    
 end
+
+Adj = Adj + Adj';
 
 end
