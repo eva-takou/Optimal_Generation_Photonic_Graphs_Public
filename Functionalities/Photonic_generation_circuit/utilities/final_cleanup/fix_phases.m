ca0f28264fcbab2cd0ca6862e9beefb6c686e20d
Eva Takou <etakou@vt.edu>
2024-06-14 22:00:29 +0300

    updates

---
+++ Functionalities/Photonic_generation_circuit/utilities/final_cleanup/fix_phases.m
@@ -0,0 +1,29 @@
+function [Tab,Circuit]=fix_phases(Tab,np,ne,Circuit,Store_Gates)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: June 14, 2024
+%--------------------------------------------------------------------------
+%
+%Function to put the phases of stabilizers to +1.
+%Input: Tab: Tableau
+%       np: # of photons
+%       ne: # of emitters
+%       Circuit: The circuit so far
+%Output: Tab: The updated tableau
+%        Circuit: The updated circuit
+
+n = np+ne;
+
+for ii=1:n
+    
+    phase = Tab(ii,end);
+    
+    if phase~=0
+        
+        Tab     = Pauli_Gate(Tab,ii,n,'X');
+        Circuit = store_gate_oper(ii,'X',Circuit,Store_Gates); 
+    end
+    
+end
+
+end
