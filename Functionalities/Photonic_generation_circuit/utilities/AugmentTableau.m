bd66701bf121df5c9be774d5e66dd01628f8f165
Eva Takou <etakou@vt.edu>
2024-06-14 21:30:49 +0300

    udpates

--- Functionalities/Photonic_generation_circuit/utilities/AugmentTableau.m
+++ Functionalities/Photonic_generation_circuit/utilities/AugmentTableau.m
@@ -1,13 +1,21 @@
 function Tab_Aug = AugmentTableau(Tab,ne)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: June 14, 2024
+%--------------------------------------------------------------------------
+%
+%Function to augment the tableau with emitters in |0> (operators of the form
+%II...ZI...II). 
+%
 %Inputs: Tab (in RREF)
 %        ne  (# of emitters needed to generate the graph)
 %        The # of emitters is determined by the height fun.
 %Output: The augmented tableau where we initialize emitters in |0>.
 
 np = (size(Tab,2)-1)/2;
-n      = np+ne;
+n  = np+ne;
 
-%Destabs/Stabs augmented in total space
+%Stabs augmented in total space
 
 Sx     = [Tab(1:np,1:np)      , zeros(np,ne,'int8')];
 Sz     = [Tab(1:np,np+1:2*np) , zeros(np,ne,'int8')]; %all np x n size
@@ -15,7 +23,7 @@ Sz     = [Tab(1:np,np+1:2*np) , zeros(np,ne,'int8')]; %all np x n size
 
 ph_S   = [Tab(1:np,2*np+1)      ; zeros(ne,1,'int8')]; % n x 1 size
 
-%initialize the emitters in |0>
+%Initialize the emitters in |0>
 
 Sx_em = zeros(ne,n,'int8');                    %ne x n in size
 Sz_em = [zeros(ne,np,'int8') , eye(ne,'int8')];     %ne x n in size
@@ -28,7 +36,5 @@ Tab_Aug = S;
 ph      = ph_S; 
 
 Tab_Aug=[Tab_Aug,ph];
-
-
 
 end
