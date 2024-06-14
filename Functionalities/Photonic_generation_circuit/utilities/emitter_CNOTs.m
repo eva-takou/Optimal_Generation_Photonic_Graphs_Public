2cf2fc82928a0c73cdc5e10311e5be9adfaba59b
Eva Takou <etakou@vt.edu>
2024-06-14 21:32:45 +0300

    updates

--- Functionalities/Photonic_generation_circuit/utilities/emitter_CNOTs.m
+++ Functionalities/Photonic_generation_circuit/utilities/emitter_CNOTs.m
@@ -1,12 +1,13 @@
 function CNOT_cnt=emitter_CNOTs(Circuit)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: June 14, 2024
+%--------------------------------------------------------------------------
+%
 %Function to count the number of CNOTs acting on emitter qubits only.
 %Inputs: Circuit: The circuit that generates the photonic graph state
-%        ne: # of emitters
-%        np: # of photons
 %Output: The CNOT count.
 
-
 CNOT_cnt       = Circuit.EmCNOTs;
-
 
 end
