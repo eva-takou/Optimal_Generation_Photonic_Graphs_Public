81a78a8032b59369ceca16590aa72801324d652c
Eva Takou <etakou@vt.edu>
2024-06-15 08:00:06 +0300

    updates

--- Functionalities/Photonic_generation_circuit/circuit_primitives/main_subroutines/PA_subroutine.m
+++ Functionalities/Photonic_generation_circuit/circuit_primitives/main_subroutines/PA_subroutine.m
@@ -1,6 +1,13 @@
 function [Tab,Circuit,graphs]=PA_subroutine(n,Tab,Circuit,graphs,photon,emitter,emitter_flag_Gate,photon_flag_Gate,Store_Graphs,Store_Gates)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: June 15, 2024
+%--------------------------------------------------------------------------
+%
 %Subroutine to do photon absorption.
-%Input:  Tab: Tableau
+%
+%Input:  n: total # of qubits
+%        Tab: Tableau
 %        Circuit: The circuit so far
 %        graphs: The graphs so far
 %        photon: The photon index \in [1,np]
@@ -8,6 +15,7 @@ function [Tab,Circuit,graphs]=PA_subroutine(n,Tab,Circuit,graphs,photon,emitter,
 %        emitter_flag_Gate: 'X','Y' or 'Z'
 %        photon_flag_Gate: 'X', 'Y' or 'Z'
 %        Store_Graphs: true or false
+%        Store_Gates: true or false
 %Output: Tab: the updated tableau
 %        Circuit: the updated circuit
 %        graphs: the updated graphs
@@ -26,8 +34,6 @@ switch emitter_flag_Gate %Bring the emitter to Z
         Circuit = store_gate_oper(emitter,'P',Circuit,Store_Gates);   
         Circuit = store_gate_oper(emitter,'H',Circuit,Store_Gates);   
    
-        
-        
 end
 
 switch photon_flag_Gate %Bring the photon to Z
@@ -54,7 +60,5 @@ if Store_Graphs
     graphs  = store_graph_transform(Get_Adjacency(Tab),...
         strcat('After CNOT_{',num2str(emitter),',',num2str(photon),'} [PA]'),graphs);
 end
-
-
 
 end
