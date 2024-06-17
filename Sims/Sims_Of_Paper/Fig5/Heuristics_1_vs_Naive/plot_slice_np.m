44b90998d4a199447addf40fb745faccebd8c330
Eva Takou <etakou@vt.edu>
2024-06-17 09:47:33 +0300

    added script for Fig5

---
+++ Sims/Sims_Of_Paper/Fig5/Heuristics_1_vs_Naive/plot_slice_np.m
@@ -0,0 +1,29 @@
+function plot_slice_np(np)
+
+close all;
+clearvars;
+
+load('500_graphs_per_np.mat')
+
+
+[~,indx_sort]=sort(CNOT_Naive(:,np));
+
+
+bar(CNOT_Naive(indx_sort,np),'k')
+
+
+hold on
+bar(CNOT_Best(indx_sort,np),'facecolor',[0.8500 0.3250 0.0980])
+legend('Naive','Heuristics $\#$1','interpreter','latex','color',...
+    'none','edgecolor','none','location','best')
+xlabel('Graph index')
+ylabel('$\#$ of emitter CNOTs','interpreter','latex')
+set(gcf,'color','w')
+set(gca,'fontsize',22)
+ylim([min(CNOT_Best(indx_sort,np))-1,...
+      max(CNOT_Naive(indx_sort,np))+10])
+
+title(['$n_p=$',num2str(np)],'interpreter','latex')  
+
+
+end
