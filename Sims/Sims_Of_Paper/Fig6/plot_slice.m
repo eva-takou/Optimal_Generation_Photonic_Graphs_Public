b3687e19c6f01b747e057d7ab302a61f3e8457bd
Eva Takou <etakou@vt.edu>
2024-06-21 20:37:30 +0300

    script to plot counts for specific np

---
+++ Sims/Sims_Of_Paper/Fig6/plot_slice.m
@@ -0,0 +1,38 @@
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: June 21, 2024
+%--------------------------------------------------------------------------
+
+close all
+
+load('500_graphs_per_np.mat')
+
+clearvars -except CNOT_Best CNOT_Naive
+
+INDX=18;
+
+
+[~,order]=sort(CNOT_Naive(:,INDX));
+
+bar(CNOT_Naive(order,INDX),'k')
+hold on
+bar(CNOT_Best(order,INDX))
+
+
+clearvars -except order INDX CNOT_Naive
+
+load('500_graphs.mat')
+hold on
+bar(CNOT_Best(order,INDX),'c')
+
+xlabel('Graph index')
+ylabel('$\#$ of emitter CNOTs','interpreter','latex')
+set(gca,'fontsize',25,'fontname','microsoft sans serif')
+set(gcf,'color','w')
+
+ylim([min(CNOT_Best(order,INDX))-3,max(CNOT_Naive(order,INDX))+2])
+
+legend('Naive','Heuristics $\#1$','Heuristics $\#2$','interpreter','latex','color','none',...
+    'edgecolor','none','location','best')
+
+title(['$n_p=$',num2str(INDX)],'interpreter','latex')
