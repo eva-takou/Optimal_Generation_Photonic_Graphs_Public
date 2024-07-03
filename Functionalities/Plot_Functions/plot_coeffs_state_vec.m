db3e2babc17d63fcbcb4ce517f373ffd771e743c
Eva Takou <etakou@vt.edu>
2024-07-03 16:48:44 +0300

    updates

--- Functionalities/Plot_Functions/plot_coeffs_state_vec.m
+++ Functionalities/Plot_Functions/plot_coeffs_state_vec.m
@@ -1,6 +1,13 @@
 function plot_coeffs_state_vec(psi,fignum)
-
-%NQ=log2(length(psi)); %Number of qubits
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: May 19, 2024
+%--------------------------------------------------------------------------
+%
+%Function to make a bar plot of the real and imaginary values of the state
+%vector coefficients.
+%Input: psi: n-qubit state
+%       fignum: number of figure to plot
 
 figure(fignum)
 subplot(2,1,1)
