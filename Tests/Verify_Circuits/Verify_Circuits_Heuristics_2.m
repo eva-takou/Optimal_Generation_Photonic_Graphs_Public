21f0f36a4627cb23ccf801fb2bdb09deb2b4c380
Eva Takou <etakou@vt.edu>
2024-07-04 17:41:56 +0300

    updates

--- Tests/Verify_Circuits/Verify_Circuits_Heuristics_2.m
+++ Tests/Verify_Circuits/Verify_Circuits_Heuristics_2.m
@@ -39,6 +39,13 @@ for iter = 1:iterMax
 
     
     Verify_Quantum_Circuit(Circ,n,ne,Target_Tableau)
-    Verify_Circuit_Forward_Order(Circ,Adj{iter},ne,'backward')
+    message_1 = Verify_Circuit_Forward_Order(Circ,Adj{iter},ne,'backward');
     
+    Circ=fix_potential_phases_forward_circuit(Circ,Adj{iter},ne,'backward');
+    
+    message_2 = Verify_Circuit_Forward_Order(Circ,Adj{iter},ne,'backward');
+    
+    if message_2
+       error('We did not fix phases') 
+    end    
 end
