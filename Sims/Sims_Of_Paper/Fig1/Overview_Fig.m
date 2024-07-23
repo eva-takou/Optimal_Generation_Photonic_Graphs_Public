a8acd0c5a6a781d1d065c7306693dff011776794
Eva Takou <etakou@vt.edu>
2024-07-23 09:48:43 +0300

    updates

--- Sims/Sims_Of_Paper/Fig1/Overview_Fig.m
+++ Sims/Sims_Of_Paper/Fig1/Overview_Fig.m
@@ -22,11 +22,23 @@ return_cond    = true; %early exit from free PA
 obj = Tableau_Class(Adj,'Adjacency');
 obj = obj.Generation_Circuit(1:np,Store_Graphs,Store_Gates,...
                              BackSubsOption,Verify_Circuit,return_cond);
+h   = obj.height;
 
+figure(2)
+plot(0:np,h,'linewidth',2,'marker','o','markerfacecolor','k',...
+    'markeredgecolor','k','color',[0.9290 0.6940 0.1250])
+xlabel('$x$','interpreter','latex')
+ylabel('$h$','interpreter','latex')
+set(gcf,'color','w')
+set(gca,'fontsize',24,'fontname','microsoft sans serif')
+          
 % --------- Extract the output: -----------------------------------                         
 Circuit = obj.Photonic_Generation_Gate_Sequence;
 graphs  = obj.Photonic_Generation_Graph_Evol;
 ne      = obj.Emitters;
+
+
+%-------- Post-process the circuit: -------------------------------
 
 Circuit = put_circuit_forward_order(Circuit);
 
@@ -34,8 +46,9 @@ layer_shift = 1;
 Init_State  = '0';
 circuitOrder = 'forward';
 
+%-------- Plot the circuit: ---------------------------------------
 
-figure(2)
+figure(3)
 draw_circuit(np,ne,Circuit,circuitOrder,layer_shift,Init_State)
 
 
@@ -45,18 +58,21 @@ pass_X_Photons      = true;
 pass_emitter_Paulis = true;
 monitor_update      = false;
 pause_time          = 0.001;
-fignum              = 3;
+fignum              = 4;
 
 Circuit = Simplify_Circuit(Circuit,np,ne,circuitOrder,ConvertToCZ,...
                           pass_X_Photons,pass_emitter_Paulis,Init_State,...
                           monitor_update,pause_time,fignum);
                       
-                      
-figure(3)                      
+%-------- Plot a simplified circuit: --------------------------------
+
+
+figure(4)                      
 draw_circuit(np,ne,Circuit,circuitOrder,layer_shift,Init_State)
 
+%-------- Plot graph evolution: -------------------------------------
 
-fignum = 4;
+fignum = 5;
 color  = [0.9290 0.6940 0.1250];
 emitters = np+1:np+ne;
 plot_graph_evol(graphs,emitters,fignum,color)
