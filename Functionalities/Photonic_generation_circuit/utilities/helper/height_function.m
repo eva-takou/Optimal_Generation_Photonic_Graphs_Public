1ae23f5a05b36eb30b91caf6ddecdd74df29946c
Eva Takou <etakou@vt.edu>
2025-02-01 21:57:00 -0500

    efficiency updates

--- Functionalities/Photonic_generation_circuit/utilities/helper/height_function.m
+++ Functionalities/Photonic_generation_circuit/utilities/helper/height_function.m
@@ -1,7 +1,7 @@
 function h=height_function(Tab_RREF,n,Request_nodes,j_indx)
 %--------------------------------------------------------------------------
 %Created by Eva Takou
-%Last modified: June 14, 2024
+%Last modified: Feb 1, 2025
 %--------------------------------------------------------------------------
 %
 %Function to calculate the entanglement entropy, given a
@@ -23,6 +23,13 @@ h = zeros(1,n+1);
 if Request_nodes
     
     j_indx=j_indx-1;
+
+   [trueX, xi] = max( Tab_RREF(:,1:n)'~=0, [], 1 );
+   xi(~trueX)=nan;
+   [trueZ, zi] = max( Tab_RREF(:,n+1:2*n)'~=0, [], 1 );
+   zi(~trueZ)=nan;
+   locs = min([xi;zi],[],1); 
+   
     
     for ii=j_indx-1:j_indx
 
@@ -42,43 +49,45 @@ if Request_nodes
 %        end
 %------------------------------------------
 
-       [trueX, xi] = max( Tab_RREF(:,1:n)'~=0, [], 1 );
-       xi(~trueX)=nan;
-       [trueZ, zi] = max( Tab_RREF(:,n+1:2*n)'~=0, [], 1 );
-       zi(~trueZ)=nan;
-       locs = min([xi;zi],[],1);
-       cnt = sum(locs>x);
-       
-       h(ii+1)=h(ii+1)+n-(x)-cnt;
-
+       cnt     = sum(locs>x);
+       h(ii+1) = h(ii+1)+n-(x)-cnt;
 
     end
     
     
 else
     
-    SX = Tab_RREF(:,1:n);
-    SZ = Tab_RREF(:,n+1:end-1);
+%     SX = Tab_RREF(:,1:n);
+%     SZ = Tab_RREF(:,n+1:end-1);
     
+    
+
+   [trueX, xi] = max( Tab_RREF(:,1:n)'~=0, [], 1 );
+   xi(~trueX)=nan;
+   [trueZ, zi] = max( Tab_RREF(:,n+1:2*n)'~=0, [], 1 );
+   zi(~trueZ)=nan;
+   locs = min([xi;zi],[],1); 
+   
     for ii=1:(n-1)
 
        x=(ii); %current node
 
-       cnt=0;
+%        cnt=0;
+% 
+%        for ll=1:n %loop through stabs
+% 
+%            xi=find(SX(ll,:),1,'first');
+%            zi=find(SZ(ll,:),1,'first');
+% 
+%            loc=min([xi,zi]);
+% 
+%            if loc>x
+%               cnt=cnt+1; 
+%            end
+% 
+%        end
 
-       for ll=1:n %loop through stabs
-
-           xi=find(SX(ll,:),1,'first');
-           zi=find(SZ(ll,:),1,'first');
-
-           loc=min([xi,zi]);
-
-           if loc>x
-              cnt=cnt+1; 
-           end
-
-       end
-       
+       cnt     = sum(locs>x);
        h(ii+1)=h(ii+1)+n-(x)-cnt;
 
     end
